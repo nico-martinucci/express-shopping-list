2e3eba9e73643830d55fc2e4eabfa068775aff51
Nico Martinucci <domenico.martinucci@gmail.com>
2023-01-04 16:25:51 -0800

    adding patch and delete routes

--- itemRoutes.js
+++ itemRoutes.js
@@ -13,23 +13,35 @@ router.get("/", function(req, res) {
 /** POST /items -- add item to list; return new item. */
 router.post("/", function(req, res) {
     items.push(req.body);
+
     res.json({ added: req.body })
 });
 
 /** GET /items/:name -- return specific items. */
 router.get("/:name", function(req, res) {
     const item = items.find(i => i.name === req.params.name);
+
     res.json(item);
 });
 
 /** PATCH /items/:name -- modify specific items; return modified item. */
 router.patch("/:name", function(req, res) {
+    const item = items.find(i => i.name === req.params.name);
     
+    for (let key in req.body) {
+        item[key] = req.body[key];
+    }
+    
+    res.json({ updated: item })
 });
 
 /** DELETE /items/:name -- delete item. */
 router.delete("/:name", function(req, res) {
+    const itemIndex = items.findIndex(i => i.name === req.params.name);
     
+    items.splice(itemIndex, 1);
+
+    res.json({ message: "Deleted" })
 });
 
 module.exports=router;
